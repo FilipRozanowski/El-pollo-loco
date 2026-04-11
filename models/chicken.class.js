1e78ec5a6c214dcbb1968a21bb08e013f76880cb
Filip Rozanowski <filip.rozanowski@gmail.com>
2026-04-11 23:21:12 +0200

    continue

---
+++ models/chicken.class.js
@@ -0,0 +1,9 @@
+class chicken extends MovableObject{
+
+
+    constructor(){
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 500;
+    }
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,21 @@
+class MovableObject {
+    x = 120;
+    y = 250;
+    img;
+    height = 200;
+    width = 100;
+
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('move right');
+    }
+
+        moveLeft(){
+        
+    }
+}
